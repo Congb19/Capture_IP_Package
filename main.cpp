b75bc2311e44eb57f2d41e29090e977e65cd2716
Congb19 <347006087@qq.com>
2019-01-11 22:13:05 +0800

    1.11 mission 1 and 2

--- main.cpp
+++ main.cpp
@@ -1,6 +1,145 @@
-#include <iostream>
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#include <iostream>
+#include <winsock2.h>
+#include <windows.h>
+#include <string.h>
+#include <mstcpip.h>
+#include <string>
+#pragma comment(lib,"Ws2_32.lib")
+using namespace std;
+//--------------------------------------------------IP首部
+struct IP_head
+{
+
+    BYTE version_len;			// IP协议版本和IP首部长度。高4位为版本，低4位为首部的长度(单位为4bytes)
+    BYTE ser_type;				// 服务类型
+    WORD wPacketLen;			// IP包总长度。包括首部，单位为byte。[Big endian]
+    WORD identification;		// 标识，一般每个IP包的序号递增。[Big endian]
+    union
+    {
+        WORD flag;				// 标志
+        WORD flagof;			// 分段偏移
+    };
+    BYTE TTL;					// 生存时间
+    BYTE Protocol_Type;			// 协议类型，见PROTOCOL_TYPE定义
+    WORD Head_checksum;			// IP首部校验和[Big endian]
+    DWORD Source_ip;			// 源地址
+    DWORD Destination_ip;       // 目的地址
+    BYTE Options;				// 选项
+};
+int cnt;						// 记录序号
+int Get_IPdata(char *buf, int len)
+{
+    int n = len;
+    if (n >= sizeof(IP_head))
+    {
+        IP_head iphead;
+        iphead = *(IP_head*)buf;
+        cout << "===================================================================" << endl ;
+        cout << "第 " << cnt++ << " 个IP数据包信息：" << endl;
+        cout << "—————————————————————————————————" << endl ;
+        cout << "版    本:" << "ipv"<< (iphead.version_len >> 4) << endl;
+        cout << "首部长度:" << ((iphead.version_len & 0x0F) << 2) << endl;//单位为4字节
+        cout << "服务类型:Priority: " << (iphead.ser_type >> 5) << ",Service: " << ((iphead.ser_type >> 1) & 0x0f) << endl;
+        cout << "总 长 度:" << ntohs(iphead.wPacketLen) << endl; //网络字节序转为主机字节序
+        cout << "标    识:" << ntohs(iphead.identification) << endl;
+        cout << "标    志:" << "DF=" << ((iphead.flag >> 14) & 0x01) << ",MF=" << ((iphead.flag >> 13) & 0x01) << endl;
+        cout << "片 偏 移:" << (iphead.flagof & 0x1fff) << endl;
+        cout << "生存时间:" << (int)iphead.TTL << endl;
+        cout << "协议类型:" << int(iphead.Protocol_Type) << endl;
+        cout << "校 验 和:" << ntohs(iphead.Head_checksum) << endl;
+        cout << "源 地 址:" << inet_ntoa(*(in_addr*)&iphead.Source_ip) << endl;
+        cout << "目的地址:" << inet_ntoa(*(in_addr*)&iphead.Destination_ip) << endl;
+
+        cout << "===================================================================" << endl << endl;
+
+
+
+
+    }
     return 0;
+}
+void AutoWSACleanup()
+{
+    ::WSACleanup();
+}
+int main()
+{
+    int n;
+    WSADATA wd;														// 这个结构被用来存储 被WSAStartup函数调用后返回的 Windows Sockets数据
+    n = WSAStartup(MAKEWORD(2, 2), &wd);							// wsastartup主要就是进行相应的socket库绑定
+    if (n)
+    {
+        cout << "WSAStartup函数错误！" << endl;
+        return -1;
+    }
+    atexit(AutoWSACleanup);											// atexit函数是一个特殊的函数，它是在正常程序退出时调用的函数，叫登记函数
+    // WSACleanup()是一个计算机函数，功能是终止Winsock 2 DLL(Ws2_32.dll) 的使用
+    //创建SOCKET
+    SOCKET sock = socket(AF_INET, SOCK_RAW, IPPROTO_IP);
+    if (sock == INVALID_SOCKET)
+    {
+        cout << WSAGetLastError();									// 该函数返回上次发生的网络错误
+        return 0;
+    }
+    //获取本机地址
+    char  name[128];
+    if (-1 == gethostname(name, sizeof(name)))						// gethostname()返回本地主机的标准主机名。
+    {
+        closesocket(sock);											// 关闭一个套接口
+        cout << WSAGetLastError();
+        return 0;
+    }
+    struct hostent * pHostent;
+    pHostent = gethostbyname(name);									// gethostbyname()返回对应于给定主机名的包含主机名字和地址信息的hostent结构的指针
+    //绑定本地地址到SOCKET句柄
+    sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_addr = *(in_addr*)pHostent->h_addr_list[0]; //IP
+    addr.sin_port = 8888; //端口，IP层端口可随意填
+    if (SOCKET_ERROR == bind(sock, (sockaddr *)&addr, sizeof(addr)))
+    {
+        closesocket(sock);
+        cout << WSAGetLastError();
+        return 0;
+    }
+
+    //设置该SOCKET为接收所有流经绑定的IP的网卡的所有数据，包括接收和发送的数据包
+    u_long sioarg = 1;
+    DWORD wt = 0;
+    if (SOCKET_ERROR == WSAIoctl(sock, SIO_RCVALL, &sioarg, sizeof(sioarg), NULL, 0, &wt, NULL, NULL))		// WSAIoctl()，控制一个套接口的模式
+    {
+        closesocket(sock);
+        cout << WSAGetLastError();
+        return 0;
+    }
+    //我们只需要接收数据，因此设置为阻塞IO，使用最简单的IO模型
+    u_long bioarg = 0;
+    if (SOCKET_ERROR == ioctlsocket(sock, FIONBIO, &bioarg))												// ioctlsocket()是一个计算机函数,功能是控制套接口的模式
+    {
+        closesocket(sock);
+        cout << WSAGetLastError();
+        return 0;
+    }
+    //开始接收数据
+    //因为前面已经设置为阻塞IO，recv在接收到数据前不会返回。
+    cnt = 1;
+    char buf[65535];
+    int len = 0;
+    string temp;
+    do
+    {
+        len = recv(sock, buf, sizeof(buf), 0);						// 不论是客户还是服务器应用程序都用recv函数从TCP连接的另一端接收数据
+        if (len > 0)
+        {
+            Get_IPdata(buf, len);
+        }
+        cout << endl << "输入回车捕获下一个数据包";
+        getline(cin, temp);
+        cout << endl;
+    } while (len > 0);
+    closesocket(sock);
+    system("pause");
 }
